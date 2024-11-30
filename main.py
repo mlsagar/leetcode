b53fb3d5f7940db1231f157809a2c57968ebbfd4
mlsagar <sagar.maharjan617571@outlook.com>
2024-11-30 08:20:21 -0600

    Add: solution of container with most water problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from two_pointers.problem5_longest_palindromic_substring.approach2 import Solution
+from two_pointers.problem11_container_with_most_water.approach2 import Solution
 
 solution = Solution()
-
-print(solution.longestPalindrome("Yaallaah"))
+height = [1,8,6,2,5,4,8,3,7]
+print(solution.maxArea(height))
