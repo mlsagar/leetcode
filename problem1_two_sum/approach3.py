8aa7c2d687b39fb677176b9189870ea6aef06354
mlsagar <sagar.maharjan617571@outlook.com>
2024-12-01 21:50:33 -0600

    Add: leetcode problem1 solution

---
+++ problem1_two_sum/approach3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        # Return an empty list if no solution is found
+        return []
