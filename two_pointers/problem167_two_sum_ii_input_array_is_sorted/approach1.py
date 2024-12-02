739fb1c382b1707b007712c1f548b0b9b0e1cf01
mlsagar <sagar.maharjan617571@outlook.com>
2024-12-02 05:56:55 -0600

    Add: add solution for two sum II

---
+++ two_pointers/problem167_two_sum_ii_input_array_is_sorted/approach1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        low = 0
+        high = len(numbers) - 1
+
+        while low < high:
+            sum = numbers[low] + numbers[high]
+
+            if sum == target:
+                return [low + 1, high + 1]
+            elif sum < target:
+                low += 1
+            else:
+                high -= 1
+
+        return []
