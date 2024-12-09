e24261795c412c82b2ec532f61c19eab32b5b445
mlsagar <sagar.maharjan617571@outlook.com>
2024-12-09 08:35:37 -0600

    Add: problem 27 solution

--- two_pointers/problem18_4sum/approach1.py
+++ two_pointers/problem18_4sum/approach1.py
@@ -1,25 +1,32 @@
 class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
         def kSum (nums: list[int], target: int, k: int) -> list[list[int]]:
-            result = []
-
-            if not nums:
-                return result
-            average_value = target // k
-            if average_value < nums[0] or nums[-1] < average_value:
-                return result
-            if k == 2:
-                return twoSum(nums, target)
+            # result = []
+            print(nums)
+            # if not nums:
+            #     return result
+            # average_value = target // k
+            # if average_value < nums[0] or nums[-1] < average_value:
+            #     return result
+            # if k == 2:
+            #     return twoSum(nums, target)
             
-            for i in range(len(nums)):
-                if i == 0 or nums[i] != nums[i - 1]:
-                    for subset in kSum(nums[i + 1:], target  - nums[i], k - 1):
-                        result.append([nums[i]] + subset)
+            # for i in range(len(nums)):
+            #     if i == 0 or nums[i] != nums[i - 1]:
+            #         print("up target", target)
+            #         print("nums", nums)
+            #         print("i", i)
+            #         print("nums[i]", nums[i])
+            #         print("Hello", target  - nums[i])
+            #         for subset in kSum(nums[i + 1:], target  - nums[i], k - 1):
+            #             print(subset)
+            #             result.append([nums[i]] + subset)
 
             return result
             
 
         def twoSum(nums: list[int], target: int) -> list[list[int]]:
+            print("target", target)
             result = []
             low = 0
             high = len(nums) - 1
@@ -37,4 +44,5 @@ class Solution:
             return result
 
         nums.sort()
-        return kSum(nums, target, 4)
+        print(nums)
+        # return kSum(nums, target, 4)
